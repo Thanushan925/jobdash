d86a2ed624e3f61df81dd26896e954f8728b273d
Thanushan Satheeskumar <80972937+Thanushan925@users.noreply.github.com>
2023-08-16 19:16:06 -0400

    Began User Registration/Login Backend Code

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import { userRouter } from './routes/users.js';
 
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/auth", userRouter);
+
+mongoose.connect("mongodb+srv://JobDashUser1:JobDashPass1@jobs.kss15ng.mongodb.net/jobs?retryWrites=true&w=majority");
+
+app.listen(3001, () => console.log("Server started!"));
